ec66685a3cf24c691bbb85c1b80fc6a4c8937db8
lrodak <rodao.lukasz@gmail.com>
2021-08-03 10:45:59 +0200

    dynamic player list

--- script.js
+++ script.js
@@ -19,7 +19,43 @@ function randomness(){
 }
 
 function playerList(){
-    // document.getElementById("player8").style.display = "none";
-    // document.getElementById("player9").style.display = "none";
-    document.getElementById("player10").style.display = "none";
+    howManyPlayersYouSay = document.getElementById("playerNumberInput").value;
+    
+    showPlayerList();
+
+    if(howManyPlayersYouSay<3){
+        document.getElementById("player3").style.display = "none";
+    } 
+    if(howManyPlayersYouSay<4){
+        document.getElementById("player4").style.display = "none";
+    } 
+    if(howManyPlayersYouSay<5){
+        document.getElementById("player5").style.display = "none";
+    } 
+    if(howManyPlayersYouSay<6){
+        document.getElementById("player6").style.display = "none";
+    }
+    if(howManyPlayersYouSay<7){
+        document.getElementById("player7").style.display = "none";
+    }
+    if(howManyPlayersYouSay<8){
+        document.getElementById("player8").style.display = "none";
+    }
+    if(howManyPlayersYouSay<9){
+         document.getElementById("player9").style.display = "none";
+    }
+    if(howManyPlayersYouSay<10){
+        document.getElementById("player10").style.display = "none";
+    }
+}
+
+function showPlayerList(){
+    document.getElementById("player3").style.display = "block";
+    document.getElementById("player4").style.display = "block";
+    document.getElementById("player5").style.display = "block";
+    document.getElementById("player6").style.display = "block";
+    document.getElementById("player7").style.display = "block";
+    document.getElementById("player8").style.display = "block";
+    document.getElementById("player9").style.display = "block";
+    document.getElementById("player10").style.display = "block";
 }
