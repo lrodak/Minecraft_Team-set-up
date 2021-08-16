f9dbc2b5c1ea98bf2e437cde98015f2e7349220f
RodakS <61910339+RodakS@users.noreply.github.com>
2021-08-16 16:20:55 +0200

    changed custom role image

--- scripts/addRoleScript.js
+++ scripts/addRoleScript.js
@@ -4,12 +4,8 @@ function addRole(){
 
     if(document.getElementById("customRole").style.display != "block"){
        document.getElementById("customRole").style.display = "block";
-       document.getElementById("customRoleLabel").innerText = addRoleVariables;
-       document.getElementById("customRoleText").style.color = "#" + ((1<<24)*Math.random() | 0).toString(16);
     }else{
        document.getElementById("customRole2").style.display = "block";
-       document.getElementById("customRole2Label").innerText = addRoleVariables;
-       document.getElementById("customRole2Text").style.color = "#" + ((1<<24)*Math.random() | 0).toString(16);
     }
 
     
