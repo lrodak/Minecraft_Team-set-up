d331404669995658e23ff7a112dea861c333b035
lrodak <rodao.lukasz@gmail.com>
2021-08-18 23:45:39 +0200

    issues fix
    - removed problem with too big input with number of players
    - fixed problem with role generator when number of players is 10

--- scripts/randomRole.js
+++ scripts/randomRole.js
@@ -1,7 +1,7 @@
 function randomness(){ 
 
     var number_of_players= document.getElementById("playerNumberInput").value;
-    var role_list=[];
+    var role_list=[0,0,0,0,0,0,0,0,0,0,0];
     var rolesPerPlayer=[0,0,0,0,0,0,0,0,0,0];
     var roles=coutRoles();
     var max_roles = Math.floor(roles/number_of_players);
@@ -41,27 +41,30 @@ function randomness(){
         alert("Too many roles!");
     }
 
-    // document.getElementById("random_roles").innerHTML = role_list[0]+" "+role_list[1]+" "+role_list[2]+" "+role_list[3]+" "+role_list[4]+" "+role_list[5]+" "+role_list[6]+" "+role_list[7]+" "+role_list[8];
-    // document.getElementById("roles_per_player").innerHTML = rolesPerPlayer[0]+" "+rolesPerPlayer[1]+" "+rolesPerPlayer[2]+" "+rolesPerPlayer[3]+" "+rolesPerPlayer[4]+" "+rolesPerPlayer[5]+" "+rolesPerPlayer[6]+" "+rolesPerPlayer[7]+" "+rolesPerPlayer[8];
+    document.getElementById("random_roles").innerHTML = role_list[0]+" "+role_list[1]+" "+role_list[2]+" "+role_list[3]+" "+role_list[4]+" "+role_list[5]+" "+role_list[6]+" "+role_list[7]+" "+role_list[8]+" "+role_list[9]+" "+role_list[10];
+    document.getElementById("roles_per_player").innerHTML = rolesPerPlayer[0]+" "+rolesPerPlayer[1]+" "+rolesPerPlayer[2]+" "+rolesPerPlayer[3]+" "+rolesPerPlayer[4]+" "+rolesPerPlayer[5]+" "+rolesPerPlayer[6]+" "+rolesPerPlayer[7]+" "+rolesPerPlayer[8]+" "+rolesPerPlayer[9];
     var src = createRoleIconsTable();
 
-    var displayed_roles = [0,0,0,0,0,0,0,0,0,0];
+    var displayed_roles = [0,0,0,0,0,0,0,0,0,0,0];
     var all_players_id_images=["player1_role1","player1_role2","player1_role3","player1_role4","player1_role5",
     "player2_role1","player2_role2","player2_role3","player2_role4","player2_role5","player3_role1","player3_role2","player3_role3","player3_role4","player3_role5",
     "player4_role1","player4_role2","player4_role3","player4_role4","player4_role5","player5_role1","player5_role2","player5_role3","player5_role4","player5_role5",
     "player6_role1","player6_role2","player6_role3","player6_role4","player6_role5","player7_role1","player7_role2","player7_role3","player7_role4","player7_role5",
     "player8_role1","player8_role2","player8_role3","player8_role4","player8_role5","player9_role1","player9_role2","player9_role3","player9_role4","player9_role5",
-    "player10_role1","player10_role2","player10_role3","player10_role4","player10_role5"]
+    "player10_role1","player10_role2","player10_role3","player10_role4","player10_role5"];
 
     for(var k=0; k<50; k++){
         document.getElementById(all_players_id_images[k]).style.display = 'none';
     }
+    var sprawdzam = [0,0,0,0,0,0,0,0,0,0,0];
 
-    for(var i=0; i<=roles; i++){
+    for(var i=0; i<roles; i++){
         document.getElementById(all_players_id_images[displayed_roles[role_list[i]]+((role_list[i]-1)*5)]).src = src[i];
         document.getElementById(all_players_id_images[displayed_roles[role_list[i]]+((role_list[i]-1)*5)]).style.display = 'inline';
         displayed_roles[role_list[i]]++;
+        sprawdzam[i]=displayed_roles[role_list[i]]+((role_list[i]-1)*5);
     }
+    document.getElementById("random_roles_src").innerHTML = sprawdzam[0]+" "+sprawdzam[1]+" "+sprawdzam[2]+" "+sprawdzam[3]+" "+sprawdzam[4]+" "+sprawdzam[5]+" "+sprawdzam[6]+" "+sprawdzam[7]+" "+sprawdzam[8]+" "+sprawdzam[9]+" "+sprawdzam[10];
 }
 
 function coutRoles(){
